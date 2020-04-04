48498fddf3a1ba5dbed17a7857b01863a4856071
Giulliany Lima Bezerra <53629399+Giulliany@users.noreply.github.com>
2020-04-04 12:59:00 -0300

    Add função de avaliação

--- ProblemaDaMochila.py
+++ ProblemaDaMochila.py
@@ -13,4 +13,23 @@ def criaPopulacao():
             
     return lista
 
-print(criaPopulacao())
+#print(criaPopulacao())
+
+def avaliacao():
+    populacao = criaPopulacao()
+    kgProduto=[7, 8, 4, 10, 4, 6, 4]
+    avaliacao=[]
+    valor = 0
+    #print(populacao)
+    
+    for i in populacao:
+        for x in range(7):
+            if i[x] == '1':
+               valor +=kgProduto[x]
+        avaliacao.append(valor)
+        valor = 0
+
+    return avaliacao
+
+#print(avaliacao())
+
