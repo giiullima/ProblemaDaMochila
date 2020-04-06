78281a085434ee0e8ec5eea5ec89068f0582f9e7
Leticia Bueno <50913322+letbueno@users.noreply.github.com>
2020-04-06 13:18:06 -0300

    Add files via upload
    seleção

--- ProblemaDaMochila.py
+++ ProblemaDaMochila.py
@@ -15,13 +15,15 @@ def criaPopulacao():
 
 #print(criaPopulacao())
 
-def avaliacao():
-    populacao = criaPopulacao()
+def criaAvaliacao(valor):
+    populacao = valor
     kgProduto=[7, 8, 4, 10, 4, 6, 4]
     benefProduto = [5, 8, 3, 2, 7, 9, 4]
     avaliacao =[]
     valorP = 0
     valorF = 0
+    item = 0
+    
     #print(populacao)
     
     for i in populacao:
@@ -29,8 +31,8 @@ def avaliacao():
             if i[x] == '1':
                valorP +=kgProduto[x]
                valorF +=benefProduto[x]
-               
-        lista =[i, valorF, valorP]
+        item+=1    
+        lista =[item, i, valorF, valorP]
         avaliacao.append(lista)
         
         valorP = 0
@@ -38,5 +40,31 @@ def avaliacao():
 
     return avaliacao
 
-print(avaliacao())
+#print(criaAvaliacao())
+
+def CriaSelecao():
+    avaliacao = criaAvaliacao(criaPopulacao())
+    avaliacao.sort(reverse=True, key=lambda x: x[1])
+    selecao=[]
+    
+    for x in range(2):
+        t=0
+        for i in avaliacao: t += i[2]
+
+        i=0
+        soma = (avaliacao[i][2] / t)
+        r = random.random()
+       
+        while soma < r:
+            i=(i+1)
+            soma += avaliacao[i][2]/t
+ 
+        selecao.append(avaliacao[i])
+        avaliacao.pop(i)
+        i=0
+    return selecao
+print(CriaSelecao())
+
+
+    
 
